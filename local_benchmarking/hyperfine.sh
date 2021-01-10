a96ef1d56844122ccf2326f0af7d2c6d6d38b0b2
C <DeveloperC@protonmail.com>
2021-01-10 17:01:14 +0000

    perf: local benchmarking

---
+++ local_benchmarking/hyperfine.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+command -v 'hyperfine' >/dev/null 2>&1 || { echo >&2 "The 'hyperfine' command is not installed. Aborting."; exit 1; }
+
+RUST_VERSION=`rustc --version | awk '{print $2}'`
+GO_VERSION=`go version | awk '{print $3}' | cut -c 3-`
+NODEJS_VERSION=`node --version | cut -c 2-`
+
+INPUT="12345"
+
+hyperfine --warmup 100 --min-runs 1000 '../rust/aws_lambda_benchmarking/target/release/aws_lambda_benchmarking --input "'$INPUT'"' --export-json "rust-$RUST_VERSION.json"
+hyperfine --warmup 100 --min-runs 1000 'node ../nodejs/aws_lambda_benchmarking/src/main.js --input "'$INPUT'"' --export-json "nodejs-$NODEJS_VERSION.json"
+
+hyperfine --warmup 100 --min-runs 1000 '../go/aws_lambda_benchmarking/aws_lambda_benchmarking --input "'$INPUT'"' --export-json "go-$GO_VERSION.json"
