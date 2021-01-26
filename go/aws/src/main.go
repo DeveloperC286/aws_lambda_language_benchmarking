484334968e4caf5807ff482ddaa963e5a2aa149d
C <DeveloperC@protonmail.com>
2021-01-26 21:38:15 +0000

    feat: AWS GO API Gateway

--- go/aws/src/main.go
+++ go/aws/src/main.go
@@ -4,20 +4,39 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"strings"
 )
 
-type LambdaEvent struct {
+type requestBody struct {
 	Input string `json:"input"`
 }
 
-func HandleRequest(ctx context.Context, lambdaEvent LambdaEvent) (string, error) {
-	hasher := sha256.New()
-	hasher.Write([]byte(lambdaEvent.Input))
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var trimmedRequestBody = strings.TrimSpace(request.Body)
+	if trimmedRequestBody != "{}" && trimmedRequestBody != "" {
+		var requestBody requestBody
+		err := json.Unmarshal([]byte(trimmedRequestBody), &requestBody)
 
-	return hex.EncodeToString(hasher.Sum(nil)), nil
+		if err != nil {
+			return events.APIGatewayProxyResponse{Body: "The request body is not valid JSON.", StatusCode: 400}, nil
+		}
+
+		if requestBody.Input != "" {
+			hasher := sha256.New()
+			hasher.Write([]byte(requestBody.Input))
+
+			return events.APIGatewayProxyResponse{Body: hex.EncodeToString(hasher.Sum(nil)), StatusCode: 200}, nil
+		}
+
+		return events.APIGatewayProxyResponse{Body: "No input within the request body.", StatusCode: 400}, nil
+	}
+
+	return events.APIGatewayProxyResponse{Body: "Empty request body.", StatusCode: 400}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handleRequest)
 }
