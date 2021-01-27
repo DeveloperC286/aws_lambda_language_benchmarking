9230317425169ca693cfa8bfcb0cedc109a6b93c
C <DeveloperC@protonmail.com>
2021-01-27 15:17:52 +0000

    feat: AWS Python3 API Gateway

--- python3/aws/src/main.py
+++ python3/aws/src/main.py
@@ -5,8 +5,47 @@ import hashlib
 
 
 def lambda_handler(event, context):
-    input = event['input'].encode('utf-8')
-    hash = hashlib.sha256(input).hexdigest()
+    body = event.get('body')
+
+    if body is not None:
+        body = body.strip()
+        if body != '' and body != '{}':
+            try:
+                body = json.loads(body)
+            except Exception:
+                return {
+                    'statusCode': 400,
+                    'body': 'The request body is not valid JSON.'
+                }
+
+            if 'input' in body:
+                input = body['input']
+
+                if type(input) == str:
+                    input = input.encode('utf-8')
+                    hash = hashlib.sha256(input).hexdigest()
+
+                    return {
+                        'statusCode': 200,
+                        'body': hash
+                    }
+
+                return {
+                    'statusCode': 400,
+                    'body': 'The input within the request body is not a String.'
+                }
+
+            return {
+                'statusCode': 400,
+                'body': 'No input within the request body.'
+            }
+
+        return {
+            'statusCode': 400,
+            'body': 'Empty request body.'
+        }
+
     return {
-        'message' : hash
+        'statusCode': 400,
+        'body': 'No request body.'
     }
---
+++ python3/terraform/api_gateway.tf
@@ -0,0 +1,41 @@
+resource "aws_api_gateway_rest_api" "api_gateway_python3_128_mb" {
+  name = "api_gateway_python3_128_mb"
+}
+
+resource "aws_api_gateway_resource" "api_gateway_python3_128_mb_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_python3_128_mb.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway_python3_128_mb.root_resource_id
+  path_part   = "hash"
+}
+
+resource "aws_api_gateway_method" "api_gateway_python3_128_mb_resource_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_python3_128_mb.id
+  resource_id   = aws_api_gateway_resource.api_gateway_python3_128_mb_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_gateway_python3_128_mb_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway_python3_128_mb.id
+  resource_id             = aws_api_gateway_resource.api_gateway_python3_128_mb_resource.id
+  http_method             = aws_api_gateway_method.api_gateway_python3_128_mb_resource_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.python3_128_mb.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway_python3_128_mb_permission" {
+  function_name = aws_lambda_function.python3_128_mb.function_name
+  principal     = "apigateway.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway_python3_128_mb.execution_arn}/*/*"
+
+  depends_on = [aws_lambda_function.python3_128_mb, aws_api_gateway_rest_api.api_gateway_python3_128_mb]
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_python3_128_mb_production" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_python3_128_mb.id
+  stage_name  = "production"
+
+  depends_on = [aws_api_gateway_integration.api_gateway_python3_128_mb_integration]
+}
