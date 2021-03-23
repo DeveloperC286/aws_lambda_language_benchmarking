1e2ae0ddfe6ab9f91555570d29e52efe95b88b2b
DeveloperC <DeveloperC@protonmail.com>
2021-03-23 23:56:38 +0000

    perf: local benchmarking + recoloured graph

--- benchmarking/local/hyperfine.sh
+++ benchmarking/local/hyperfine.sh
@@ -2,18 +2,20 @@
 
 command -v 'hyperfine' >/dev/null 2>&1 || { echo >&2 "The 'hyperfine' command is not installed. Aborting."; exit 1; }
 
-DATE=`date '+%d-%m-%Y'`
 RUST_VERSION=`rustc --version | awk '{print $2}'`
 GO_VERSION=`go version | awk '{print $3}' | cut -c 3-`
 NODEJS_VERSION=`node --version | cut -c 2-`
 PYTHON3_VERSION=`python3 --version | awk '{print $2}'`
 
+DATE=`date '+%d-%m-%Y'`
 INPUT="12345"
+WARMUP=100
+ITERATIONS=1000
 
-hyperfine --warmup 100 --min-runs 1000 '../../rust/local/target/release/aws_lambda_benchmarking --input "'$INPUT'"' --export-json "rust-$RUST_VERSION-$DATE.json"
+hyperfine --warmup $WARMUP --min-runs $ITERATIONS '../../rust/local/target/release/aws_lambda_language_benchmarking --input "'$INPUT'"' --export-json "rust-$RUST_VERSION-$DATE.json"
 
-#hyperfine --warmup 100 --min-runs 1000 'node ../../nodejs/local/src/main.js --input "'$INPUT'"' --export-json "nodejs-$NODEJS_VERSION-$DATE.json"
+hyperfine --warmup $WARMUP --min-runs $ITERATIONS 'node ../../nodejs/local/src/main.js --input "'$INPUT'"' --export-json "nodejs-$NODEJS_VERSION-$DATE.json"
 
-#hyperfine --warmup 100 --min-runs 1000 '../../go/local/aws_lambda_benchmarking --input "'$INPUT'"' --export-json "go-$GO_VERSION-$DATE.json"
+hyperfine --warmup $WARMUP --min-runs $ITERATIONS '../../go/local/aws_lambda_language_benchmarking --input "'$INPUT'"' --export-json "go-$GO_VERSION-$DATE.json"
 
-#hyperfine --warmup 100 --min-runs 1000 'python3 ../../python3/local/src/main.py --input "'$INPUT'"' --export-json "python3-$PYTHON3_VERSION-$DATE.json"
+hyperfine --warmup $WARMUP --min-runs $ITERATIONS 'python3 ../../python3/local/src/main.py --input "'$INPUT'"' --export-json "python3-$PYTHON3_VERSION-$DATE.json"
--- benchmarking/local/local_benchmarking_graphing.py
+++ benchmarking/local/local_benchmarking_graphing.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import sys
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
 import argparse
 import json
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import re
+import sys
 from os import path
 
 
@@ -17,7 +18,7 @@ def main(argv):
     args = parser.parse_args()
 
     if not path.isfile(args.nodejs):
-        print("The input NodeJS file '{}' does not exist or is not a file.".format(args.input))
+        print("The input Node.js file '{}' does not exist or is not a file.".format(args.input))
         sys.exit(2)
 
     if not path.isfile(args.rust):
@@ -28,45 +29,50 @@ def main(argv):
         print("The input Go file '{}' does not exist or is not a file.".format(args.input))
         sys.exit(2)
 
-    if not path.isfile(args.go):
+    if not path.isfile(args.python3):
         print("The input Python3 file '{}' does not exist or is not a file.".format(args.input))
         sys.exit(2)
 
     patches = []
 
     with open(args.nodejs) as f:
-        results =  json.load(f)['results'][0]
+        results = json.load(f)['results'][0]
         times = results['times']
-        colour = 'limegreen'
+        colour = 'lime'
         plt.plot(list(range(1, len(times) + 1)), times, color=colour)
-        patches.append(mpatches.Patch(color=colour, label='NodeJS'))
+        nodejs_version = re.split('-', args.nodejs)[1]
+        patches.append(mpatches.Patch(color=colour, label='Node.js ' + nodejs_version))
 
     with open(args.rust) as f:
-        results =  json.load(f)['results'][0]
+        results = json.load(f)['results'][0]
         times = results['times']
-        colour = 'orange'
+        colour = 'orangered'
         plt.plot(list(range(1, len(times) + 1)), times, color=colour)
-        patches.append(mpatches.Patch(color=colour, label='Rust'))
+        rust_version = re.split('-', args.rust)[1]
+        patches.append(mpatches.Patch(color=colour, label='Rust ' + rust_version))
 
     with open(args.go) as f:
-        results =  json.load(f)['results'][0]
+        results = json.load(f)['results'][0]
         times = results['times']
-        colour = 'cyan'
+        colour = 'lightskyblue'
         plt.plot(list(range(1, len(times) + 1)), times, color=colour)
-        patches.append(mpatches.Patch(color=colour, label='Go'))
+        go_version = re.split('-', args.go)[1]
+        patches.append(mpatches.Patch(color=colour, label='Go ' + go_version))
 
     with open(args.python3) as f:
-        results =  json.load(f)['results'][0]
+        results = json.load(f)['results'][0]
         times = results['times']
         colour = 'yellow'
         plt.plot(list(range(1, len(times) + 1)), times, color=colour)
-        patches.append(mpatches.Patch(color=colour, label='Python3'))
-
+        python3_version = re.split('-', args.python3)[1]
+        patches.append(mpatches.Patch(color=colour, label='Python3 ' + python3_version))
 
     plt.legend(handles=patches)
     plt.ylabel('Time (Seconds)')
     plt.xlabel('Iteration')
-    plt.grid(True)
+    plt.grid(b=True, which='major', linestyle='-')
+    plt.minorticks_on()
+    plt.grid(b=True, which='minor', linestyle='--', alpha=0.5)
     plt.show()
 
 
